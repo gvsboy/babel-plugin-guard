1265850dc1ff644195f717172b5d001e9a1c3f93
Kevin Mikles <kmikles@linkedin.com>
2016-03-20 17:05:49 -0700

    Initial commit. A silly little plugin with a complete testing solution. Time to get rad.

---
+++ test/index.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+import { expect } from 'chai';
+import { transformFileSync } from 'babel-core';
+
+const rootDir = path.dirname(fs.realpathSync(__filename));
+const fixturesDir = path.join(rootDir, 'fixtures');
+const expectedDir = path.join(rootDir, 'expected');
+const plugin = path.join(rootDir, '../src');
+
+function transform(fixture) {
+  const file = path.join(fixturesDir, fixture);
+  return transformFileSync(file, {
+    babelrc: false,
+    plugins: [ plugin ]
+  }).code;
+}
+
+function getExpected(fixture) {
+  const file = path.join(expectedDir, fixture);
+  return fs.readFileSync(file, {
+    encoding: 'utf8'
+  });
+}
+
+describe('babel-plugin-guard', () => {
+
+  it('works!', () => {
+    expect(true).to.be.ok;
+  });
+
+  describe('transformations', () => {
+    fs.readdirSync(fixturesDir).forEach((fixture) => {
+      it(`correctly matches ${fixture} to it's expected source`, () => {
+        let transformed = transform(fixture);
+        let expected = getExpected(fixture);
+        expect(transformed.trim()).to.equal(expected.trim());
+      });
+    });
+  });
+
+});
